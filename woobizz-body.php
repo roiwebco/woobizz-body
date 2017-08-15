79cb141c3b9d8c31c8c041ab6b30c54a739545df
roiweb <info@roiweb.co>
2017-08-15 08:59:02 +0200

    15-08-2017 version 1.0.1
    Reorganize files & scripts

--- woobizz-body.php
+++ woobizz-body.php
@@ -1,30 +1,30 @@
 <?php
 /**
- * Plugin Name: Woobizz Body 
- * Plugin URI: https://woobizz.com
- * Description: Woocommerce Storefront Body Customiser
- * Author: Woobizz
- * Author URI: https://woobizz.com
- * Version: 1.0.0
- * Text Domain: woobizz-body
- * Domain Path: /languages/
- */
- /** The prefix Name*/
-define('WOOBIZZBODY', 'woobizzbody');
-
-/** Include the Whitelabel plugin */
-include('admin/body-admin-page.php' );
-	
-/** Include the $options array */
-include( 'admin/body-options.php' );
-
-/** Include the Custom CSS Variables */
-include( 'admin/body-functions.php' );
-
-/** Create the Options Page */
-$options_page = new WoobizzBodyOptions( 'WooBizz Body', 'woobizz-body', WOOBIZZBODY, 'admin.php', null, 'read', null, true, false, true, $options );
-function woobizz_body_submenu(){
-	//add_submenu_page($parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function);
-	add_submenu_page( 'woobizz', '', 'Woobizz Body','manage_options', 'woobizz-body');
+* Plugin Name: Woobizz Body
+* Plugin URI: https://woobizz.com
+* Description: Woocommerce BodyCustomiser
+* Author: Woobizz
+* Author URI: https://woobizz.com
+* Version: 1.0.1
+* Text Domain: woobizz-body
+* Domain Path: /lang/
+*/
+//Prevent direct acces
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+//Load translation
+add_action( 'plugins_loaded', 'woobizz_body_load_textdomain' );
+function woobizz_body_load_textdomain() {
+  load_plugin_textdomain( 'woobizz-body', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
 }
-add_action( 'admin_menu', 'woobizz_body_submenu',20);
+// The prefix Name
+define('WOOBIZZBODY', 'woobizzbody');
+// Include the Whitelabel plugin
+include('admin/body-admin-page.php' );
+// Include the $options array 
+include( 'admin/body-options.php' );
+// Include all sections php files
+foreach ( glob( plugin_dir_path( __FILE__ ) . "/admin/sections/*.php" ) as $file ) {
+    include_once $file;
+}
+// ADD OPTIONS AND SUBMENU
+$options_page = new WoobizzBodyOptions( 'WooBizz Body', 'woobizz-body', WOOBIZZBODY, 'woobizz', null, 'read', null, true, false, true, $options );
